9abe52d0700c10c49cee4483794619fa0f4c58ba
Susie_Sun <8315104+susie-sun@user.noreply.gitee.com>
2021-07-25 11:57:27 +0800

    First commit.

--- app/src/main/java/com/example/kuouweather/MainActivity.java
+++ app/src/main/java/com/example/kuouweather/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.kuouweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
-
 public class MainActivity extends AppCompatActivity {
-
+    DBOpenHelper dbOpenHelper = new DBOpenHelper(this,"data.db",null,1);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SQLiteDatabase sqLiteDatabase = dbOpenHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
